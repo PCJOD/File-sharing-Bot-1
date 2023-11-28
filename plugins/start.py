d7818754c33840ebc2c20ab5989cab7c84d11336
Pc <126058657+PCJOD@users.noreply.github.com>
2023-11-28 08:39:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,9 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("🔰𝐀𝐛𝐨𝐮𝐭 𝐌𝐞🔰", callback_data = "about"),
-                    InlineKeyboardButton("❌𝐂𝐥𝐨𝐬𝐞❌", callback_data = "close")
+                    InlineKeyboardButton("❌𝐂𝐥𝐨𝐬𝐞❌", callback_data = "close"),
+                    btn.insert(0, [InlineKeyboardButton("🔰 Movie Group 🔰", url="https://t.me/pcmoviegroup")])
+        
                 ]
             ]
         )
